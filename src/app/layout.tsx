047079739050f39f1586cfb4bb0f923f1dd85318
nturf <thestraybyte@gmail.com>
2025-06-15 10:03:13 +0530

    clerk auth added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -24,6 +32,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
+        <ClerkProvider>
         <html lang="en">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
                 <ThemeProvider
@@ -36,5 +45,6 @@ export default function RootLayout({
                 </ThemeProvider>
             </body>
         </html >
+        </ClerkProvider>
     );
 }
